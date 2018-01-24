e3d6d952d57c3b8b2976602a59862ff30d9bf8e2
corentingay <corentin.gay060@gmail.com>
2018-01-24 12:24:15 +0100

    script: add line for accessing X from the container
    Signed-off-by: corentingay <corentin.gay060@gmail.com>

--- docker.sh
+++ docker.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+ xhost +local:docker
+
+# We forward the X server so that the container can use a GUI
 docker build -t ada .
-docker run -v /usr/gnat:/usr/gnat -v ~/emu_ada:/home/ferryk/ada -t -i ada:latest
+docker run --cap-add=all --privileged -e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /usr/gnat:/usr/gnat \
+-v ~/courses/ada/ada_epita:/home/gps/ada \
+-t -i ada:latest
