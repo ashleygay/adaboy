c6c45334b03ce69de7bc666c66bf04e5e393fb9f
corentingay <corentin.gay060@gmail.com>
2018-01-31 18:24:38 +0100

    misc: move functions into main script file
    Signed-off-by: corentingay <corentin.gay060@gmail.com>

--- launch.sh
+++ launch.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-source "functions.sh"
-CUR_PATH=`pwd`
+try_tar ()
+{
+	echo "Checking if $1 need decompressing"
+	_DIR=${1%.tar.*}
+	if [ ! -d $_DIR ]; then
+		echo "Decompressing $1"
+		tar -xvf $1
+	fi
+}
 
+CUR_PATH=`pwd`
 GNAT_GZ="gnat-gpl-2017-arm-elf-linux-bin.tar.gz"
 LINARO_GZ="gcc-linaro-6.3.1-2017.05-i686_arm-eabi.tar.xz"
+
+# If toolchains are not present we download then
+{
+	if [ ! -f $GNAT_GZ ]; then
+		wget "mirrors.cdn.adacore.com/art/591c6413c7a447af2deed0e3"
+	fi
+
+	if [ ! -f $LINARO_GZ ]; then
+
+		wget "http://releases.linaro.org/components/toolchain/binaries/6.3-2017.02/arm-eabi/gcc-linaro-6.3.1-2017.02-i686_arm-eabi.tar.xz"
+	fi
+}
 
 # We unpack the toolchains here if they have not
 # been previously unpacked
@@ -21,6 +41,7 @@ LINARO_GZ="gcc-linaro-6.3.1-2017.05-i686_arm-eabi.tar.xz"
 }
 
 # We forward the X server so that the container can use a GUI
+# TODO: add linaro toolchain
 {
 	docker run -e DISPLAY=$DISPLAY\
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
